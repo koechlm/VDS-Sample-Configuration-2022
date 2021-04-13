d78894c576b6d8c453c6d5d4118a52fea8bcac85
koechlm <markus.koechl@autodesk.com>
2021-04-13 18:26:54 +0200

    added 2022 RTM configuration files

---
+++ VDS-2022-Manufacturing-Sample/Vault/addinVault/Default.ps1
@@ -0,0 +1,359 @@
+﻿
+#[System.Reflection.Assembly]::LoadFrom("C:\Program Files (x86)\Autodesk\Autodesk Vault 2022 SDK\bin\Autodesk.Connectivity.WebServices.dll")
+#$cred = New-Object Autodesk.Connectivity.WebServicesTools.UserPasswordCredentials("localhost", "Vault", "Administrator", "")
+#$vault = New-Object Autodesk.Connectivity.WebServicesTools.WebServiceManager($cred)
+
+#this function will be called to check if the Ok button can be enabled
+function ActivateOkButton
+{
+	return Validate;
+}
+
+# sample validation function
+# finds all function definition with names beginning with
+# ValidateFile, ValidateFolder and ValidateTask respectively
+# these funcions should return a boolean value, $true if the Property is valid
+# $false otherwise
+# As soon as one property validation function returns $false the entire Validate function will return $false
+function Validate
+{
+    if ($Prop["_ReadOnly"].Value){
+		return $false
+	}
+	$mWindowName = $dsWindow.Name
+	switch($mWindowName)
+	{
+		"FileWindow"
+		{
+			foreach ($func in dir function:ValidateFile*) { if(!(&$func)) { return $false } }
+			return $true
+		}
+		"FolderWindow"
+		{
+			foreach ($func in dir function:ValidateFolder*) { if(!(&$func)) { return $false } }
+			return $true
+		}
+		"CustomObjectWindow"
+		{
+			foreach ($func in dir function:ValidateCustomObject*) { if(!(&$func)) { return $false } }
+			return $true
+		}
+		default { return $true }
+	}    
+}
+
+# sample validation function for the FileName property
+# if the File Name is empty the validation will fail
+function ValidateFileName
+{
+	if($Prop["_FileName"].Value -or !$dsWindow.FindName("DSNumSchmsCtrl").NumSchmFieldsEmpty)
+	{
+		return $true;
+	}
+	return $false;
+}
+
+function ValidateFolderName
+{
+	if($Prop["_FolderName"].Value -or !$dsWindow.FindName("DSNumSchmsCtrl").NumSchmFieldsEmpty)
+	{
+		return $true;
+	}
+	return $false;
+}
+
+function ValidateCustomObjectName
+{
+	if($Prop["_CustomObjectName"].Value -or !$dsWindow.FindName("DSNumSchmsCtrl").NumSchmFieldsEmpty)
+	{
+		return $true;
+	}
+	return $false;
+}
+
+function InitializeTabWindow
+{
+	#$dsDiag.ShowLog()
+	#$dsDiag.Inspect()
+}
+
+function InitializeWindow
+{	
+	#begin rules applying commonly
+    InitializeWindowTitle
+    InitializeNumSchm
+    InitializeCategory
+	#end rules applying commonly
+	$mWindowName = $dsWindow.Name
+	switch($mWindowName)
+	{
+		"FileWindow"
+		{
+			#rules applying for File
+		}
+		"FolderWindow"
+		{
+			#rules applying for Folder
+		}
+		"CustomObjectWindow"
+		{
+			#rules applying for Custom Object
+		}
+	}
+}
+
+function InitializeWindowTitle()
+{
+    $mWindowName = $dsWindow.Name
+	switch($mWindowName)
+	{
+		"FileWindow"
+		{
+			#rules applying for File
+			$dsWindow.Title = SetWindowTitle $UIString["LBL24"] $UIString["LBL25"] $Prop["_FileName"].Value
+		}
+		"FolderWindow"
+		{
+			#rules applying for Folder
+			$dsWindow.Title = SetWindowTitle $UIString["LBL29"] $UIString["LBL30"] $Prop["_FolderName"].Value
+		}
+		"CustomObjectWindow"
+		{
+			#rules applying for Custom Object
+			$dsWindow.Title = SetWindowTitle $UIString["LBL61"] $UIString["LBL62"] $Prop["_CustomObjectName"].Value
+		}
+	}
+}
+
+function InitializeCategory()
+{
+    $mWindowName = $dsWindow.Name
+	switch($mWindowName)
+	{
+		"FileWindow"
+		{
+			if ($Prop["_CreateMode"].Value)
+			{
+				if ($Prop["_IsOfficeClient"].Value)
+				{
+					$Prop["_Category"].Value = $UIString["CAT2"]
+				}
+				else
+				{
+					$Prop["_Category"].Value = $UIString["CAT1"]
+				}
+			}			
+		}
+		"FolderWindow"
+		{
+			if ($Prop["_CreateMode"].Value)
+			{
+				$Prop["_Category"].Value = $UIString["CAT5"]
+			}
+		}
+		"CustomObjectWindow"
+		{
+			if ($Prop["_CreateMode"].Value)
+			{
+				$Prop["_Category"].Value = $Prop["_CustomObjectDefName"].Value
+			}
+		}
+	}
+}
+
+
+function InitializeNumSchm()
+{
+	#Adopted from a DocumentService call, which always pulls FILE class numbering schemes
+	$global:numSchems = @($vault.NumberingService.GetNumberingSchemes('FILE', 'Activated')) 
+    $Prop["_Category"].add_PropertyChanged({
+        if ($_.PropertyName -eq "Value")
+        {
+            $numSchm = $numSchems | where {$_.Name -eq $Prop["_Category"].Value}
+            if($numSchm)
+			{
+                $Prop["_NumSchm"].Value = $numSchm.Name
+            }
+        }		
+    })
+}
+
+
+function SetWindowTitle($newFile, $editFile, $name)
+{
+	if ($Prop["_CreateMode"].Value)
+	{
+		$windowTitle = $newFile		
+	}
+	elseif ($Prop["_EditMode"].Value)
+	{
+		if ($Prop["_ReadOnly"].Value) {
+		  $windowTitle = "$name -- $($UIString["LBL26"])"
+		}
+		else {			
+		  $windowTitle = "$editFile - $name"
+		}
+	}
+	return $windowTitle
+}
+
+function OnLogOn
+{
+	#Executed when User logs on Vault
+	#$vaultUsername can be used to get the username, which is used in Vault on login
+}
+function OnLogOff
+{
+	#Executed when User logs off Vault
+}
+
+function GetTitleWindow
+{
+	$message = "Autodesk Data Standard - Create/Edit "+$Prop["_FileName"]
+	return $message
+}
+
+#fired when the file selection changes
+function OnTabContextChanged
+{
+	$xamlFile = [System.IO.Path]::GetFileName($VaultContext.UserControl.XamlFile)
+	
+	if ($VaultContext.SelectedObject.TypeId.SelectionContext -eq "FileMaster" -and $xamlFile -eq "CAD BOM.xaml")
+	{
+		$fileMasterId = $vaultContext.SelectedObject.Id
+		$file = $vault.DocumentService.GetLatestFileByMasterId($fileMasterId)
+		$bom = @(GetFileBOM($file.id))
+		$dsWindow.FindName("bomList").ItemsSource = $bom
+	}
+	if ($VaultContext.SelectedObject.TypeId.SelectionContext -eq "ItemMaster" -and $xamlFile -eq "Associated Files.xaml")
+	{
+		$items = $vault.ItemService.GetItemsByIds(@($vaultContext.SelectedObject.Id))
+		$item = $items[0]
+		$itemids = @($item.Id)
+		$assocFiles = @(GetAssociatedFiles $itemids $([System.IO.Path]::GetDirectoryName($VaultContext.UserControl.XamlFile)))
+		$dsWindow.FindName("AssoicatedFiles").ItemsSource = $assocFiles
+	}
+}
+
+function GetNewCustomObjectName
+{
+	$dsDiag.Trace(">> GetNewCustomObjectName")
+	if($dsWindow.FindName("DSNumSchmsCtrl").NumSchmFieldsEmpty)
+	{	
+		$dsDiag.Trace("read text from TextBox CUSTOMOBJECTNAME")
+		$customObjectName = $dsWindow.FindName("CUSTOMOBJECTNAME").Text
+		$dsDiag.Trace("customObjectName = $customObjectName")
+	}
+	else{
+		$dsDiag.Trace("-> GenerateNumber")
+		$customObjectName = $Prop["_GeneratedNumber"].Value
+		$dsDiag.Trace("customObjectName = $customObjectName")
+	}
+	$dsDiag.Trace("<< GetNewCustomObjectName $customObjectName")
+	return $customObjectName
+}
+
+#Constructs the filename(numschems based or handtyped)and returns it.
+function GetNewFileName
+{
+	$dsDiag.Trace(">> GetNewFileName")
+	if($dsWindow.FindName("DSNumSchmsCtrl").NumSchmFieldsEmpty)
+	{	
+		$dsDiag.Trace("read text from TextBox FILENAME")
+		$fileName = $dsWindow.FindName("FILENAME").Text
+		$dsDiag.Trace("fileName = $fileName")
+	}
+	else{
+		$dsDiag.Trace("-> GenerateNumber")
+		$fileName = $Prop["_GeneratedNumber"].Value
+		$dsDiag.Trace("fileName = $fileName")
+	}
+	$newfileName = $fileName + $Prop["_FileExt"].Value
+	$dsDiag.Trace("<< GetNewFileName $newfileName")
+	return $newfileName
+}
+
+function GetNewFolderName
+{
+	$dsDiag.Trace(">> GetNewFolderName")
+	if($dsWindow.FindName("DSNumSchmsCtrl").NumSchmFieldsEmpty)
+	{	
+		$dsDiag.Trace("read text from TextBox FOLDERNAME")
+		$folderName = $dsWindow.FindName("FOLDERNAME").Text
+		$dsDiag.Trace("folderName = $folderName")
+	}
+	else{
+		$dsDiag.Trace("-> GenerateNumber")
+		$folderName = $Prop["_GeneratedNumber"].Value
+		$dsDiag.Trace("folderName = $folderName")
+	}
+	$dsDiag.Trace("<< GetNewFolderName $folderName")
+	return $folderName
+}
+
+# This function can be used to force a specific folder when using "New Standard File" or "New Standard Folder" functions.
+# If an empty string is returned the selected folder is used
+# ! Do not remove the function
+function GetParentFolderName
+{
+	$folderName = ""
+	return $folderName
+}
+
+function GetCategories
+{
+	if ($dsWindow.Name -eq "FileWindow")
+	{
+		return $vault.CategoryService.GetCategoriesByEntityClassId("FILE", $true)
+	}
+	elseif ($dsWindow.Name -eq "FolderWindow")
+	{
+		return $vault.CategoryService.GetCategoriesByEntityClassId("FLDR", $true)
+	}
+	elseif ($dsWindow.Name -eq "CustomObjectWindow")
+	{
+		return $vault.CategoryService.GetCategoriesByEntityClassId("CUSTENT", $true)
+	}
+}
+
+function GetNumSchms
+{
+	if ($Prop["_CreateMode"].Value)
+	{
+		try
+		{
+			if ($numSchems.Count -gt 1)
+			{
+				$numSchems = $numSchems | Sort-Object -Property IsDflt -Descending
+			}
+			return $numSchems
+		}
+		catch [System.Exception]
+		{		
+			#[System.Windows.MessageBox]::Show($error)
+		}
+	}
+}
+
+#Workaround for Property names containing round brackets
+#Xaml fails to parse
+function ItemTitle
+{
+    if ($Prop)
+	{
+       $val = $Prop["_XLTN_TITLE_ITEM_CO"].Value
+	   return $val
+    }
+}
+
+#Workaround for Property names containing round brackets
+#Xaml fails to parse
+function ItemDescription
+{
+	if($Prop)#Tab gets loaded before the SelectionChanged event gets fired resulting with null Prop. Happens when the vault is started with Change Order as the last view.
+    {
+       $val = $Prop["_XLTN_DESCRIPTION_ITEM_CO"].Value
+	   return $val
+    }
+}
+ 
---
+++ VDS-2022-Manufacturing-Sample/Vault/addinVault/FileBOM.ps1
@@ -0,0 +1,77 @@
+﻿
+Add-Type @'
+public class myBom
+{
+	public string Position;
+	public string PartNumber;
+	public string ComponentType;
+	public float Quantity;
+	public string Name;
+	public byte[] Thumbnail;
+	public string Title;
+	public string Description;
+	public string Material;
+}
+'@
+
+function GetFileBOM($fileID)
+{
+	#Get Thumbnail PropertyDefinition 
+	$propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId('FILE')
+	$thumbnailPropDef = $propDefs | Where-Object {$_.SysName -eq 'Thumbnail'}
+
+	$dsDiag.Trace(">> Starting GetFileBOM($fileID)")
+	$bom = $vault.DocumentService.GetBOMByFileId($fileID)
+	$cldIds =@()
+	$bom.InstArray | Where-Object { $_.ParId -eq 0 } | ForEach-Object { 
+		$CldId = $_.CldId
+		$comp = $bom.CompArray | Where-Object { $_.Id -eq $CldId }
+		$cldIds += $comp.XRefId
+	}
+	$dsDiag.Trace("   cldIds: "+$cldIds.Count)
+	$bomItems = @()
+	if($cldIds.Count -gt 0) #the file contains BOM information, so continue
+	{
+		$CldBoms = $vault.DocumentService.GetBOMByFileIds($cldIds)
+		$schm = $bom.SchmArray | Where-Object { $_.SchmTyp -eq "Structured" -and $_.RootCompId -eq 0}
+		$cldBomCounter = 0
+
+		$bom.InstArray | Where-Object { $_.ParId -eq 0 } | ForEach-Object {
+			$bomItem = New-Object myBom
+			$CldId = $_.CldId
+            if ($_.QuantOverde -eq -1)
+			{
+                $bomItem.Quantity = $_.Quant
+            }
+            else {
+                $bomItem.Quantity = $_.QuantOverde
+            }
+			$comp = $bom.CompArray | Where-Object { $_.Id -eq $CldId }
+
+			$occur = $bom.SchmOccArray | Where-Object { $_.SchmId -eq $schm.Id -and $_.CompId -eq $CldId }
+			$bomItem.Position = $occur.DtlId
+
+			$cldBom = $CldBoms[$cldBomCounter++]
+			$bomItem.Name = $cldBom.CompArray[0].Name
+			$bomItem.ComponentType = $cldBom.CompArray[0].CompTyp
+			$PropPartNumber = $cldBom.PropArray | Where-Object { $_.dispName -eq "Part Number"}
+			$prop = ($cldBom.CompAttrArray | Where-Object { $_.PropId -eq $PropPartNumber.Id}) | Select -First 1
+			$bomItem.PartNumber = $prop.Val
+			$bomItems += $bomItem
+			#add Inventor default BOM columns
+			$thumbnailProp = $vault.PropertyService.GetProperties('FILE', @($cldIds[$cldBomCounter - 1]), @($thumbnailPropDef.Id))[0]
+			$bomItem.Thumbnail = $thumbnailProp.Val
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Title"}
+			$prop = $cldBom.CompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select -First 1
+			$bomItem.Title = $prop.Val
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Description"}
+			$prop = $cldBom.CompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select -First 1
+			$bomItem.Description = $prop.Val
+			$m_Prop = $cldBom.PropArray | Where-Object { $_.dispName -eq "Material"}
+			$prop = $cldBom.CompAttrArray | Where-Object { $_.PropId -eq $m_Prop.Id} | Select -First 1
+			$bomItem.Material = $prop.Val
+		}
+	}
+	$dsDiag.Trace("<< Ending GetFileBOM with $($bomItems.Count) items found")
+	return $bomItems
+}
